1a222ec8fd0a67ae567743381205f36d653c4576
Vladimir Sadovnikov <sadko4u@gmail.com>
2026-06-13 17:13:11 +0300

    Version up

--- project.mk
+++ project.mk
@@ -23,4 +23,4 @@ ARTIFACT_ID                 = LSP_AUDIO_PIPEWIRE_LIB
 ARTIFACT_NAME               = lsp-audio-pipewire-lib
 ARTIFACT_DESC               = Audio backend for PipeWire audio subsystem
 ARTIFACT_HEADERS            = lsp-plug.in
-ARTIFACT_VERSION            = 1.0.1
+ARTIFACT_VERSION            = 1.0.2-devel
